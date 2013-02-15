2520bade70ede5b1963214ffa567f56accb3c83f
marian kramarczyk <mkramarc@gmail.com>
2013-02-15 16:23:20 -0500

    sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user   
       flash[:success] = "Welcome to the Interactive!"       
-      render 'static_pages/home'
+      redirect_to @user
      else
        render 'new'
      end
